1ce9173426ed8858b88031b378900a50f84801e5
cytxiwai <98063373+cytxiwai@users.noreply.github.com>
2022-10-15 00:15:42 -0700

    rework tuiter - added index.js to explore pg

--- public/labs/a5/todos/index.js
+++ public/labs/a5/todos/index.js
@@ -1,2 +1,13 @@
-alert('Hello World!');
+//alert('Hello World!');
+
+import TodoList from "./TodoList.js";
+
+$('#wd-todo').append(`
+   <div class="container">
+       <h1>Todo example</h1>
+       ${TodoList()}
+   </div>
+`);
+
+
 
