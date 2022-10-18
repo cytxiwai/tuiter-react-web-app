42134bc9615a05f5247a4219ebbc61bbaed0549e
cytxiwai <98063373+cytxiwai@users.noreply.github.com>
2022-10-17 23:57:36 -0700

    a6 react started

---
+++ src/labs/index.js
@@ -0,0 +1,15 @@
+function Labs() {
+    return(
+    <div>
+        <h1>Labs</h1>
+        <h2>Assignment 6 </h2>
+        <h3>Exercise 1 </h3>
+        <h3>Exercise 2 </h3>
+        <h3>Exercise 3 </h3>
+        <h2>Assignment 7 </h2>
+        <h2>Assignment 8 </h2>
+        <h2>Assignment 9 </h2>
+    </div>
+    )
+}
+export default Labs;
